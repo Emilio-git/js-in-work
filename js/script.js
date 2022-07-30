3530fb597676b81d8075d0c2a2638fbc3c3d113f
Emil <e.yakoff4@gmail.com>
2022-07-30 17:07:42 +0300

    added modal window

--- js/script.js
+++ js/script.js
@@ -102,4 +102,38 @@ document.addEventListener('DOMContentLoaded', () => {
    }
 
    setClock('.timer', deadline);
+
+   // Modal
+
+   const modalTrigger = document.querySelectorAll('[data-modal]'),
+         modalCloseBtn = document.querySelector('[data-close]'),
+         modal = document.querySelector('.modal');
+
+   modalTrigger.forEach(item => {
+      item.addEventListener('click', () => {
+         modal.classList.toggle('show');
+         document.body.style.overflow = 'hidden';
+      });
+   });
+
+   function closeModal() {
+      modal.classList.toggle('show');
+      document.body.style.overflow = '';
+   }
+
+   modalCloseBtn.addEventListener('click', closeModal);
+
+   modal.addEventListener('click', (e) => {
+      const target = e.target;
+
+      if (target === modal) {
+         closeModal();
+      }
+   });
+
+   document.addEventListener('keydown', (e) => {
+      if (e.code === 'Escape' && modal.classList.contains('show')) {
+         closeModal();
+      }
+   });
 });
